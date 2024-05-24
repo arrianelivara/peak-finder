eb83aa3b3ff10b24c9f408d6bdedf30dcc0332ab
Arriane E. Livara <arriane.livara@gmail.com>
2024-05-24 08:18:10 +0800

    Update AutoCompleteSearch.jsx

--- src/AutoCompleteSearch.jsx
+++ src/AutoCompleteSearch.jsx
@@ -1,7 +1,7 @@
 import React, {useRef, useEffect, useState} from 'react';
 import { useMapsLibrary } from '@vis.gl/react-google-maps';
 
-export const AutoCompleteSearch = ({ onPlaceSelect, selectedPlace }) => {
+export const AutoCompleteSearch = ({ onPlaceSelect, selectedPlace, setFormattedAddress }) => {
   const [placeAutocomplete, setPlaceAutocomplete] = useState(null);
   const inputRef = useRef(null);
   const places = useMapsLibrary('places');
@@ -24,12 +24,29 @@ export const AutoCompleteSearch = ({ onPlaceSelect, selectedPlace }) => {
       const lat = place.geometry.location.lat();
       const lng = place.geometry.location.lng();
       onPlaceSelect({ lat, lng });
+
+      const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${ import.meta.env.VITE_API_KEY}`;
+      fetch(geocodeUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'OK') {
+                    const formattedAddress = data.results[0].formatted_address;
+                    setFormattedAddress(formattedAddress)
+                } else {
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+        });
     });
+
   }, [onPlaceSelect, placeAutocomplete]);
 
   return (
     <div className="autocomplete-container">
-      <input ref={inputRef} value={selectedPlace} className="w-full mb-3 p-3 rounded-md border border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none shadow-md"/>
+      <input ref={inputRef} 
+          // value={selectedPlace} 
+          className="w-full mb-3 p-3 rounded-md border border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none shadow-md"/>
     </div>
   );
 };
